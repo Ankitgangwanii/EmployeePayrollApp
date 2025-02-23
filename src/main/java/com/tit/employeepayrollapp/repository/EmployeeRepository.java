e3becaf35c45e3dfebe0e221cbccf51db9a849ee
Ankit Gangwani <Ankitgangwani160@gmail.com>
2025-02-23 21:36:55 +0530

    [Ankit] Add . UC-1 feature Employee Payroll Spring project, replacing JSONServer with Spring backend, tested via CURL

---
+++ src/main/java/com/tit/employeepayrollapp/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.tit.employeepayrollapp.repository;
+
+
+import com.tit.employeepayrollapp.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
